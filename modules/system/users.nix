a297a5e1acb2e366f40c891bfb0539347c07f3fb
Elliana Perry <elliana.perry@gmail.com>
2023-07-15 15:22:17 -0400

    password shenanigans

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -13,12 +13,14 @@ in
         shell = pkgs.zsh;
         initialHashedPassword =
           "$6$PGLB6RuHp8APuNbK$wijx7Ggs//iS6e79/gITwnCYk4v.pFDrqReqDUa1Ft/nV5eNeY9gMJeewFMY4HNixL5dtTDhQ1aQIYUsEAZlp.";
+        hashedPassword = config.users.users."root".initialHashedPassword;
       };
       elliana = {
         description = "Elliana Perry";
         isNormalUser = true;
         initialHashedPassword = 
           "$6$XbN67zeoi4SK8f7W$Tj5lFhMjNbBSvbBF1.zAgOM0jxqMmMNy/mhhXoNONXW2MZdW1rHReyAk5MTaOZpfEvXx7wes6xyt21/93T5s1.";
+        hashedPassword = config.users.users."elliana".initialHashedPassword;
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID1WrsxCqVK8oVf4whQs3W+vAV8ioV4bVr7399SSj4xC"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII+gTodZbPhGkhk3DJDn4MKTURN0pW1jnKKj4QYKJ58j"
