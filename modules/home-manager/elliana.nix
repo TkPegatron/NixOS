3d052e449aff801651cec08020aed50c11eddaf6
Elliana Perry <elliana.perry@gmail.com>
2023-07-16 13:30:20 -0400

    corrected input

--- modules/home-manager/elliana.nix
+++ modules/home-manager/elliana.nix
@@ -15,10 +15,12 @@
         }
         { #--{GPG User-Specific Config}---------------------#
             modules.gpg.enable = true;
-            programs.gpg.publicKeys.elliana = {
-                trust = "ultimate";
-                text = builtins.readFile ./gpg/elliana.pub;
-            };
+            programs.gpg.publicKeys = [
+                {
+                    trust = "ultimate";
+                    text = builtins.readFile ./gpg/elliana.pub;
+                }
+            ];
             services.gpg-agent.sshKeys = [
                 "ED2EDC2C8563ABB9404C5877DB56182523676CD1"
                 "420F1135CC6DA2519C39DFB6C2A05B70B83FE16F"
