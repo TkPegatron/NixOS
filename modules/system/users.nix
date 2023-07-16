00a8fd431af5be9a700ce314437bbc0f1eb19ac9
Elliana Perry <elliana.perry@gmail.com>
2023-07-15 21:47:11 -0400

    import current starship theme

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -22,6 +22,7 @@ in
           "$6$Zby1R5nVdcA/3L2A$OY4SUiTiQreKuXtgx8SqAiVgaSTlV7g/Pd4ANYDF9pInsBwyurU6DxzGSUM0/COkUkhMEz3M2lAoUAaDnhcyQ.";
         hashedPassword = config.users.users."elliana".initialHashedPassword;
         openssh.authorizedKeys.keys = [
+          # Ideally these could be retreived from a git server
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8u8SQls2xm80xrDKGufi9mfrngmjLiapsRnMh1QITi"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIYbWTYTT64VUBOABSKuCBN5H1mE/NgTT0K1Xjzszbn"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKRwLHJDFwHodUByK0hO0B3ofWGE9fWp13RhFZAu9xc"
