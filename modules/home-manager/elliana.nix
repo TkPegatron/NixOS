e6cf8e0d0735bfa61405a337ebe6e3fe1d08cf75
Elliana Perry <elliana.perry@gmail.com>
2023-07-16 12:44:53 -0400

    gpg configuration options

--- modules/home-manager/elliana.nix
+++ modules/home-manager/elliana.nix
@@ -1,6 +1,16 @@
 { config, lib, inputs, ...}: {
     imports = [ ./common.nix ];
-    config.modules = {
-        gpg.enable = true;
-    };
+    config = lib.mkMerge [
+        {# MODULES
+            modules = {
+                gpg.enable = true;
+            };
+        }
+        {
+            services.gpg-agent.sshKeys = [
+                "ED2EDC2C8563ABB9404C5877DB56182523676CD1"
+                "420F1135CC6DA2519C39DFB6C2A05B70B83FE16F"
+            ];
+        }
+    ];
 }
