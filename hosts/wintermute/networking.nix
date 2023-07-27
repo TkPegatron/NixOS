4f9a8ef7c206361fa30c571539b121575c419631
Elliana Perry <elliana.perry@gmail.com>
2023-07-26 20:56:41 -0400

    placeolder for peers

--- hosts/wintermute/networking.nix
+++ hosts/wintermute/networking.nix
@@ -12,6 +12,12 @@
             ips = [ "172.22.0.130" ];
             listenPort = 51820;
             privateKeyFile = config.age.secrets.wireguard_key.path;
+            #peers = [
+            #    { # home firewall
+            #        publicKey = "{client public key}";
+            #        allowedIPs = [ "10.0.0.2/32" ];
+            #    }
+            #];
         };
     };
 }
