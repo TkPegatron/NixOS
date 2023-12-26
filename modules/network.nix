691af72b0af131b0a4255d7af5802f4db7b8922e
Elliana Perry <elliana.perry@gmail.com>
2023-12-25 19:08:37 -0500

    feat: deploy tls trust

--- modules/network.nix
+++ modules/network.nix
@@ -1,8 +1,27 @@
 { config, pkgs, lib, inputs, ... }: {
     config = lib.mkMerge ([
         { #--{Network Configuration}-----------#
+            security.pki.certificates = [
+                ''
+                Elliana Perry
+                ==============
+                -----BEGIN CERTIFICATE-----
+                MIIB4DCCAWYCFFY6UiHBqKEcpxxxo4e/+86dtwxWMAoGCCqGSM49BAMCMFQxCzAJ
+                BgNVBAYTAlVTMQ0wCwYDVQQIDARPaGlvMQ8wDQYDVQQHDAZNYXVtZWUxJTAjBgNV
+                BAMMHEVsbGlhbmEgUGVycnkgQ2VydCBBdXRob3JpdHkwHhcNMjMxMTIyMjIwNzUz
+                WhcNNDMxMTI0MjIwNzUzWjBUMQswCQYDVQQGEwJVUzENMAsGA1UECAwET2hpbzEP
+                MA0GA1UEBwwGTWF1bWVlMSUwIwYDVQQDDBxFbGxpYW5hIFBlcnJ5IENlcnQgQXV0
+                aG9yaXR5MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAELHgnHqKNZs6J7isa26l6BtUE
+                Ov9GCb0VKZel+NttlHRAHXj9RdaVZAu1WROun5PBtyq8hik402PG7BGWqTJcvCsp
+                zzB2sW2mmUfvg/X2b3gKof9huigSA5x14VzNf4LSMAoGCCqGSM49BAMCA2gAMGUC
+                MDh2NRy+vdps1JwsowN5odNautrNCHzlSOWJVFjXHl7bQJgPtCa7zThwNzYLakfx
+                lAIxANW89E7najBVzQ3GRwHvJYHE5QlYIgtVv6kFjEZErnPikShQt5HUm3B19puQ
+                XKNuXw==
+                -----END CERTIFICATE-----
+                ''
+            ];
             environment.systemPackages = with pkgs; [
-                ivpn ivpn-service doggo
+                ivpn ivpn-service doggo mtr
             ];
             #? Consider switching to networkd
             networking = {
