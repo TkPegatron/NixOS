ae00d5738bf3aba1fdc53451f1cb00eb7976b2db
Elliana Perry <elliana.perry@gmail.com>
2024-03-23 22:43:23 -0400

    feat: testing pasma6

--- modules/extra/plasma.nix
+++ modules/extra/plasma.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }: {
   imports = [ ./init5.nix ];
   # Configure NixOS to start GDM
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-    #displayManager.defaultSession = "plasmawayland";
-  };
+  services.xserver.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+  services.xserver.displayManager.sddm.wayland.enable = true;
 }
