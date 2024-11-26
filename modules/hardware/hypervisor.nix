d226f0382f1d35ea1b2645d202e00dd167023c27
Elliana Perry <elliana.perry@gmail.com>
2024-11-26 13:43:00 -0500

    tweak: additional libvirt settings

--- modules/hardware/hypervisor.nix
+++ modules/hardware/hypervisor.nix
@@ -1,3 +1,17 @@
-_: {
-  virtualisation.libvirtd.enable = true;
+{ pkgs, ... }: {
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 }
