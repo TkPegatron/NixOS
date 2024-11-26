9983687572eeeec80bf64db9d83b08bd80b61da8
Elliana Perry <elliana.perry@gmail.com>
2024-11-26 13:41:04 -0500

    fix: updated naming convention

--- modules/desktop/plasma.nix
+++ modules/desktop/plasma.nix
@@ -2,9 +2,9 @@
   # Configure NixOS to start GDM
   services.xserver.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasma";
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.defaultSession = "plasma";
+  services.displayManager.sddm.wayland.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
   ];
