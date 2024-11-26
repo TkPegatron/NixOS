1dddfbe8a7876034275d0e75e8c754187a63b98b
Elliana Perry <elliana.perry@gmail.com>
2024-11-26 13:42:34 -0500

    tweak: yubikey components

--- modules/extra/yubikey.nix
+++ modules/extra/yubikey.nix
@@ -9,7 +9,6 @@
     gitAndTools.git-extras
     gnupg
     gpg-scripts
-    #(haskell.lib.justStaticExecutables haskellPackages.hopenpgp-tools)
     paperkey
     parted
     pcsclite
@@ -21,9 +20,27 @@
     yubikey-manager
     yubikey-personalization
     drduh-gpg-guide
+
+    # Yubikey - Openssl engine
+    openssl
+    opensc
+    libp11
+    p11-kit
   ];
   services.pcscd.enable = true;
   services.udev.packages = [
     pkgs.yubikey-personalization
   ];
+  environment.etc."ssl/openssl.cnf".text = ''
+    openssl_conf = openssl_init
+    [openssl_init]
+      engines=engine_section
+    [engine_section]
+      pkcs11 = pkcs11_section
+    [pkcs11_section]
+      engine_id = pkcs11
+      dynamic_path = ${pkgs.libp11}/lib/engines/libpkcs11.so
+      MODULE_PATH = ${pkgs.opensc}/lib/pkcs11/opensc-pkcs11.so
+      init = 0
+    '';
 }
