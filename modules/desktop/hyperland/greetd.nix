4f57b8d3c927e4cb500199ed02bace38ef3e6f2c
Elliana Perry <elliana.perry@gmail.com>
2024-11-30 13:19:09 -0500

    tweak: automatically log into session when using hyprland

--- modules/desktop/hyperland/greetd.nix
+++ modules/desktop/hyperland/greetd.nix
@@ -1,12 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, user, ... }: {
   # Enable Display Manager
   services.greetd = {
     enable = true;
-    settings = {
-      default_session = {
+    settings = rec {
+      login_required = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
         user = "greeter";
       };
+      autologin = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = user.username;
+      };
+      default_session = autologin;
     };
   };
   environment.systemPackages = with pkgs; [
