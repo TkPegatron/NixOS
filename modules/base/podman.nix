bd8b6552590bdf85d48927c6dd41cf0243cf1913
Elliana Perry <elliana.perry@gmail.com>
2025-02-01 13:05:53 -0500

    feat: expanded available podman featureset

--- modules/base/podman.nix
+++ modules/base/podman.nix
@@ -1,7 +1,15 @@
-_: {
+{ pkgs, ... }: {
+  virtualisation.oci-containers.backend = "podman";
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     dockerSocket.enable = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
+  environment.systemPackages = with pkgs; [
+    dive
+    podman-tui
+    podman-compose
+    docker-compose
+  ];
 }
