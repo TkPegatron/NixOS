bb8abe572df8e8f3cddbcd5a11fd3d86494a7498
Elliana Perry <elliana.perry@gmail.com>
2025-02-05 16:50:39 -0500

    fix: reconfigure filesystems

--- systems/htpc/filesystems.nix
+++ systems/htpc/filesystems.nix
@@ -1,6 +1,11 @@
 { config, ... }: {
-    fileSystems.'/' = {
-        device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/12CE-A600";
+        fsType = "vfat";
+        options = ["noatime"];
+    }
+    fileSystems."/" = {
+        device = "/dev/disk/by-uuid/5c148f7d-a921-4b61-a792-93808f12499b";
         fsType = "ext4";
         options = ["noatime"];
     }
